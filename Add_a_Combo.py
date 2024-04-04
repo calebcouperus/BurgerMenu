640a539923bca6a12e04f82a092436f423f7b1da
calebcouperus <124112022+calebcouperus@users.noreply.github.com>
2024-04-05 11:44:25 +1300

    first commit

---
+++ Add_a_Combo.py
@@ -0,0 +1,10 @@
+combos = {'value': {'beef burger': '5.69', 'fries': '1.00', 'fizzy drink':
+        '1.00'},
+          'cheezy': {'cheeseburger': '6.69', 'fries': '1.00', 'fizzy '
+                                                                 'drink':
+        '1.00'},
+          'super':{'cheeseburger': '6.69', 'large fries': '2.00',
+                      'smoothie': '2.00'}}
+
+def add():
+
